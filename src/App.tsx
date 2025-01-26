542fdf8130f4dc4bef65141a338fd36458b7e467
Thomas Nguyen <thomasnguyen729@gmail.com>
2025-01-25 23:33:45 -0600

    Allow partial tags

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 const App: React.FC = () => {
   const [multilineString, setMultilineString] = useState<string>('');
   const [codingStyle, setCodingStyle] = useState<string>('<++>');
+  const [tagPosition, setTagPosition] = useState<string>('full');
 
   const SECONDARY_COLOR = 'blue'
   const PRIMARY_COLOR = 'green'
@@ -24,6 +25,7 @@ const App: React.FC = () => {
               wordIndex={wordIndex} 
               updateWord={updateWord} 
               codingStyle={codingStyle}
+              tagPosition={tagPosition}
             />
           ))}
         </div>
@@ -43,75 +45,108 @@ const App: React.FC = () => {
 
   return (
     <>
+      <Typography variant='h3' textAlign='center' mb='20px'>Lesson Plan Helper</Typography>
 
-    <Typography variant='h3' textAlign='center' mb='20px'>Lesson Plan Helper</Typography>
+      <Grid2 container spacing={3}>
+        <Grid2 size={6}>
+          <TextField 
+            multiline 
+            rows={30} 
+            value={multilineString}
+            onChange={(e) => setMultilineString(e.target.value)} 
+            style={{ marginBottom: '20px' }}
+            fullWidth
+          />
+          <Grid2
+            container
+            sx={{
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+            style={{ marginBottom: '20px' }}
+          >
+            <Button
+              variant="contained"
+              onClick={() => setCodingStyle('<++>')}
+              style={
+                codingStyle === '<++>'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              +word+
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setCodingStyle('<-->')}
+              style={
+                codingStyle === '<-->'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              -word-
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setCodingStyle('<!!>')}
+              style={
+                codingStyle === '<!!>'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              !word!
+            </Button>
+          </Grid2>
+          <Grid2
+            container
+            sx={{
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+            style={{ marginBottom: '20px' }}
+          >
+            <Button
+              variant="contained"
+              onClick={() => setTagPosition('full')}
+              style={
+                tagPosition === 'full'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              Full
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setTagPosition('front')}
+              style={
+                tagPosition === 'front'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              Front
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => setTagPosition('back')}
+              style={
+                tagPosition === 'back'
+                  ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
+                  : { backgroundColor: SECONDARY_COLOR, color: 'white' }
+              }
+            >
+              Back
+            </Button>
+          </Grid2>
+        </Grid2>
 
-    <Grid2 container spacing={3}>
-      
-
-    <Grid2 size={6}>
-      <TextField 
-        multiline 
-        rows={30} 
-        value={multilineString}
-        onChange={(e) => setMultilineString(e.target.value)} 
-        style={{ marginBottom: '20px' }}
-        fullWidth
-
-      />
-      <Grid2
-        container
-        sx={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-        style={{ marginBottom: '20px' }}
-      >
-        <Button
-          variant="contained"
-          onClick={() => setCodingStyle('<++>')}
-          style={
-            codingStyle === '<++>'
-              ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
-              : { backgroundColor: SECONDARY_COLOR, color: 'white' }
-          }
-        >
-          +word+
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => setCodingStyle('<-->')}
-          style={
-            codingStyle === '<-->'
-            ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
-            : { backgroundColor: SECONDARY_COLOR, color: 'white' }
-          }
-        >
-          -word-
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => setCodingStyle('<!!>')}
-          style={
-            codingStyle === '<!!>'
-            ? { backgroundColor: PRIMARY_COLOR, color: 'white' }
-            : { backgroundColor: SECONDARY_COLOR, color: 'white' }
-          }
-        >
-          !word!
-        </Button>
+        <Grid2 size={6}>
+          {createButtons(multilineString)}
+        </Grid2>
       </Grid2>
-
-      </Grid2>
-
-
-      <Grid2 size={6}>
-      {createButtons(multilineString)}
-
-      </Grid2>
-
-      </Grid2>
-
     </>
   );
 };
@@ -122,34 +157,52 @@ interface WordButtonProps {
   wordIndex: number;
   updateWord: (lineIndex: number, wordIndex: number, newWord: string) => void;
   codingStyle: string;
+  tagPosition: string;
 }
 
-const WordButton: React.FC<WordButtonProps> = ({ word, lineIndex, wordIndex, updateWord, codingStyle }) => {
-  const [wordValue, setWordValue] = useState(word);
-
+const WordButton: React.FC<WordButtonProps> = ({ word, lineIndex, wordIndex, updateWord, codingStyle, tagPosition }) => {
   const handleClick = () => {
-    const punctuationMatch = wordValue.match(/[.,!?:;]$/);
+    const punctuationMatch = word.match(/[.,!?:;]$/);
     const punctuation = punctuationMatch ? punctuationMatch[0] : '';
-    let cleanWord = punctuation ? wordValue.slice(0, -1) : wordValue;
+    let cleanWord = punctuation ? word.slice(0, -1) : word;
 
     const startTag = codingStyle.slice(0, 2);
     const endTag = codingStyle.slice(2);
 
-    if (cleanWord.startsWith(startTag) && cleanWord.endsWith(endTag)) {
-      cleanWord = cleanWord.slice(2, -2); // Remove coding tags
-    } else {
-      cleanWord = `${startTag}${cleanWord}${endTag}`;
+    switch(tagPosition) {
+      case 'full':
+        if (cleanWord.startsWith(startTag) && cleanWord.endsWith(endTag)) {
+          cleanWord = cleanWord.slice(2, -2); // Remove coding tags
+        } else {
+          cleanWord = `${startTag}${cleanWord}${endTag}`;
+        }
+        break;
+      case 'front':
+        if (cleanWord.startsWith(startTag)) {
+          cleanWord = cleanWord.slice(2);
+        } else {
+          cleanWord = `${startTag}${cleanWord}`;
+        }
+        break;
+      case 'back':
+        if (cleanWord.endsWith(endTag)) {
+          cleanWord = cleanWord.slice(0, -2);
+        } else {
+          cleanWord = `${cleanWord}${endTag}`;
+        }
+        break;
+      default:
+        break;
     }
 
     const newWord = `${cleanWord}${punctuation}`;
-    setWordValue(newWord);
     updateWord(lineIndex, wordIndex, newWord);
   };
 
   return (
-    <button onClick={handleClick} style={{ marginRight: '5px' }}>
-      {wordValue}
-    </button>
+    <Button onClick={handleClick} style={{ marginRight: '5px' }} variant='outlined' size='small'>
+      {word}
+    </Button>
   );
 };
 
